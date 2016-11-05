43fee70020dbeb0cae5f5691deb074362d83c7dd
nadee158 <nadee158@gmail.com>
2016-11-05 13:34:39 +0530

    ReferRejectGrid implemented

---
+++ scss-parent/scss-core/src/main/java/com/privasia/scss/core/repository/ReferRejectDetailRepository.java
@@ -0,0 +1,16 @@
+package com.privasia.scss.core.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.privasia.scss.core.model.ReferRejectDetail;
+
+public interface ReferRejectDetailRepository extends PagingAndSortingRepository<ReferRejectDetail, Long> {
+
+  List<ReferRejectDetail> findByReferReject_StatusCodeOrderByReferReject_ReferDateTimeDesc(String statusCode,
+      Pageable pageRequest);
+
+
+}
---
+++ scss-parent/scss-refer/src/main/java/com/privasia/scss/refer/service/ReferRejectService.java
@@ -0,0 +1,40 @@
+package com.privasia.scss.refer.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.privasia.scss.core.exception.ResultsNotFoundException;
+import com.privasia.scss.core.model.ReferRejectDetail;
+import com.privasia.scss.core.repository.ReferRejectDetailRepository;
+import com.privasia.scss.refer.dto.ReferRejectListDto;
+
+@Service("referRejectService")
+public class ReferRejectService {
+
+  @Autowired
+  private ReferRejectDetailRepository referRejectDetailRepository;
+
+  @Transactional()
+  public List<ReferRejectListDto> getReferRejectList(String statusCode, int page, int pageSize) {
+    PageRequest pageRequest = new PageRequest(page, pageSize);
+
+    List<ReferRejectDetail> list = referRejectDetailRepository
+        .findByReferReject_StatusCodeOrderByReferReject_ReferDateTimeDesc(statusCode, pageRequest);
+    if (!(list == null || list.isEmpty())) {
+      List<ReferRejectListDto> dtoList = new ArrayList<ReferRejectListDto>();
+      list.forEach(referRejectDetail -> {
+        dtoList.add(new ReferRejectListDto(referRejectDetail));
+      });
+      return dtoList;
+    } else {
+      throw new ResultsNotFoundException("No refer rejects were found!");
+    }
+  }
+
+}
