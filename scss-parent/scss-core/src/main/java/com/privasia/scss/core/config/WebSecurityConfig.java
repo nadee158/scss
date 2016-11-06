314642287d693c53950773f5a85b231506e8f5ee
nadee158 <nadee158@gmail.com>
2016-11-07 00:04:26 +0530

    modified to run on external tomcat - chnaged master to war

--- scss-parent/scss-core/src/main/java/com/privasia/scss/core/config/WebSecurityConfig.java
+++ scss-parent/scss-core/src/main/java/com/privasia/scss/core/config/WebSecurityConfig.java
@@ -45,79 +45,82 @@ import com.privasia.scss.core.security.util.MD5PasswordEncoder;
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	public static final String JWT_TOKEN_HEADER_PARAM = "X-Authorization";
-    public static final String FORM_BASED_LOGIN_ENTRY_POINT = "/scss/auth/login";
-    public static final String TOKEN_BASED_AUTH_ENTRY_POINT = "/scss/**/";
-    public static final String TOKEN_REFRESH_ENTRY_POINT = "/scss/auth/token";
-    
-    @Autowired private RestAuthenticationEntryPoint authenticationEntryPoint;
-    @Autowired private AuthenticationSuccessHandler successHandler;
-    @Autowired private AuthenticationFailureHandler failureHandler;
-    @Autowired private SCSSAuthenticationProvider scssAuthenticationProvider;
-    @Autowired private JwtAuthenticationProvider jwtAuthenticationProvider;
-    
-    @Autowired private TokenExtractor tokenExtractor;
-    
-    @Autowired private AuthenticationManager authenticationManager;
-    
-    @Autowired private ObjectMapper objectMapper;
-        
-    @Bean
-    protected LoginProcessingFilter buildLoginProcessingFilter() throws Exception {
-    	LoginProcessingFilter filter = new LoginProcessingFilter(FORM_BASED_LOGIN_ENTRY_POINT, successHandler, failureHandler, objectMapper);
-        filter.setAuthenticationManager(this.authenticationManager);
-        return filter;
-    }
-    
-    @Bean
-    protected JwtTokenAuthenticationProcessingFilter buildJwtTokenAuthenticationProcessingFilter() throws Exception {
-        List<String> pathsToSkip = Arrays.asList(TOKEN_REFRESH_ENTRY_POINT, FORM_BASED_LOGIN_ENTRY_POINT);
-        SkipPathRequestMatcher matcher = new SkipPathRequestMatcher(pathsToSkip, TOKEN_BASED_AUTH_ENTRY_POINT);
-        JwtTokenAuthenticationProcessingFilter filter 
-            = new JwtTokenAuthenticationProcessingFilter(failureHandler, tokenExtractor, matcher);
-        filter.setAuthenticationManager(this.authenticationManager);
-        return filter;
-    }
 
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-    
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(scssAuthenticationProvider);
-        auth.authenticationProvider(jwtAuthenticationProvider);
-    }
-    
-    @Bean
-    protected PasswordEncoder passwordEncoder() {
-        return new MD5PasswordEncoder();
-    }
+  public static final String JWT_TOKEN_HEADER_PARAM = "X-Authorization";
+  public static final String FORM_BASED_LOGIN_ENTRY_POINT = "/scss/auth/login";
+  public static final String TOKEN_BASED_AUTH_ENTRY_POINT = "/scss/**/";
+  public static final String TOKEN_REFRESH_ENTRY_POINT = "/scss/auth/token";
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-        .csrf().disable() // We don't need CSRF for JWT based authentication
-        .exceptionHandling()
-        .authenticationEntryPoint(this.authenticationEntryPoint)
-        
-        .and()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+  @Autowired
+  private RestAuthenticationEntryPoint authenticationEntryPoint;
+  @Autowired
+  private AuthenticationSuccessHandler successHandler;
+  @Autowired
+  private AuthenticationFailureHandler failureHandler;
+  @Autowired
+  private SCSSAuthenticationProvider scssAuthenticationProvider;
+  @Autowired
+  private JwtAuthenticationProvider jwtAuthenticationProvider;
 
-        .and()
-            .authorizeRequests()
-                .antMatchers(FORM_BASED_LOGIN_ENTRY_POINT).permitAll() // Login end-point
-                .antMatchers(TOKEN_REFRESH_ENTRY_POINT).permitAll() // Token refresh end-point
-                .antMatchers("/console").permitAll() // H2 Console Dash-board - only for testing
-        .and()
-            .authorizeRequests()
-                .antMatchers(TOKEN_BASED_AUTH_ENTRY_POINT).authenticated()  // Protected API End-points
-        .and()
-            .addFilterBefore(buildLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class)
-            .addFilterBefore(buildJwtTokenAuthenticationProcessingFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
+  @Autowired
+  private TokenExtractor tokenExtractor;
+
+  @Autowired
+  private AuthenticationManager authenticationManager;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @Bean
+  protected LoginProcessingFilter buildLoginProcessingFilter() throws Exception {
+    LoginProcessingFilter filter =
+        new LoginProcessingFilter(FORM_BASED_LOGIN_ENTRY_POINT, successHandler, failureHandler, objectMapper);
+    filter.setAuthenticationManager(this.authenticationManager);
+    return filter;
+  }
+
+  @Bean
+  protected JwtTokenAuthenticationProcessingFilter buildJwtTokenAuthenticationProcessingFilter() throws Exception {
+    List<String> pathsToSkip = Arrays.asList(TOKEN_REFRESH_ENTRY_POINT, FORM_BASED_LOGIN_ENTRY_POINT);
+    SkipPathRequestMatcher matcher = new SkipPathRequestMatcher(pathsToSkip, TOKEN_BASED_AUTH_ENTRY_POINT);
+    JwtTokenAuthenticationProcessingFilter filter =
+        new JwtTokenAuthenticationProcessingFilter(failureHandler, tokenExtractor, matcher);
+    filter.setAuthenticationManager(this.authenticationManager);
+    return filter;
+  }
+
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  protected void configure(AuthenticationManagerBuilder auth) {
+    auth.authenticationProvider(scssAuthenticationProvider);
+    auth.authenticationProvider(jwtAuthenticationProvider);
+  }
+
+  @Bean
+  protected PasswordEncoder passwordEncoder() {
+    return new MD5PasswordEncoder();
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable() // We don't need CSRF for JWT based authentication
+        .exceptionHandling().authenticationEntryPoint(this.authenticationEntryPoint)
+
+        .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        .and().authorizeRequests().antMatchers(FORM_BASED_LOGIN_ENTRY_POINT).permitAll() // Login
+                                                                                         // end-point
+        .antMatchers(TOKEN_REFRESH_ENTRY_POINT).permitAll() // Token refresh end-point
+        .antMatchers("/console").permitAll() // H2 Console Dash-board - only for testing
+        .and().authorizeRequests().antMatchers(TOKEN_BASED_AUTH_ENTRY_POINT).authenticated() // Protected
+                                                                                             // API
+                                                                                             // End-points
+        .and().addFilterBefore(buildLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class)
+        .addFilterBefore(buildJwtTokenAuthenticationProcessingFilter(), UsernamePasswordAuthenticationFilter.class);
+  }
 
 }
