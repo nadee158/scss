d1492bc3a6e00dfc9a5cf88ab8e17642e485ae6f
nadee158 <nadee158@gmail.com>
2016-11-07 21:21:27 +0530

    @value added to jwt settings

--- scss-parent/scss-core/src/main/java/com/privasia/scss/core/model/Role.java
+++ scss-parent/scss-core/src/main/java/com/privasia/scss/core/model/Role.java
@@ -4,6 +4,7 @@
 package com.privasia.scss.core.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.AttributeOverride;
 import javax.persistence.AttributeOverrides;
@@ -12,6 +13,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
@@ -25,65 +27,68 @@ import com.privasia.scss.core.util.constant.RoleType;
  *
  */
 @Entity
-@Table(name = "SCSS_ROLE", uniqueConstraints = { @UniqueConstraint(columnNames = "ROL_NAME") })
-@AttributeOverrides({ @AttributeOverride(name = "addBy", column = @Column(name = "ADD_BY")),
-		@AttributeOverride(name = "updateBy", column = @Column(name = "UPDATE_BY")),
-		@AttributeOverride(name = "dateTimeAdd", column = @Column(name = "DATETIME_ADD")),
-		@AttributeOverride(name = "dateTimeUpdate", column = @Column(name = "DATETIME_UPDATE")) })
+@Table(name = "SCSS_ROLE", uniqueConstraints = {@UniqueConstraint(columnNames = "ROL_NAME")})
+@AttributeOverrides({@AttributeOverride(name = "addBy", column = @Column(name = "ADD_BY")),
+    @AttributeOverride(name = "updateBy", column = @Column(name = "UPDATE_BY")),
+    @AttributeOverride(name = "dateTimeAdd", column = @Column(name = "DATETIME_ADD")),
+    @AttributeOverride(name = "dateTimeUpdate", column = @Column(name = "DATETIME_UPDATE"))})
 public class Role extends AuditEntity implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_SCSS_ROLE")
-	@SequenceGenerator(name = "SEQ_SCSS_ROLE", sequenceName = "ROL_ROLEID_SEQ")
-	@Column(name = "ROL_ROLEID_SEQ")
-	private Long roleID;
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_SCSS_ROLE")
+  @SequenceGenerator(name = "SEQ_SCSS_ROLE", sequenceName = "ROL_ROLEID_SEQ")
+  @Column(name = "ROL_ROLEID_SEQ")
+  private Long roleID;
 
-	@Column(name = "ROL_NAME")
-	private String roleName;
+  @Column(name = "ROL_NAME")
+  private String roleName;
 
-	@Column(name = "ROL_TYPE")
-	@Type(type = "com.privasia.scss.core.util.enumusertype.RoleTypeEnumUserType")
-	private RoleType roleType;
+  @Column(name = "ROL_TYPE")
+  @Type(type = "com.privasia.scss.core.util.enumusertype.RoleTypeEnumUserType")
+  private RoleType roleType;
 
-	@Column(name = "ROL_SUPERVISOR_FLAG")
-	@Type(type = "yes_no")
-	private boolean supervisorFlag;
+  @Column(name = "ROL_SUPERVISOR_FLAG")
+  @Type(type = "yes_no")
+  private boolean supervisorFlag;
 
-	public Long getRoleID() {
-		return roleID;
-	}
+  @OneToMany(mappedBy = "roleRightsID")
+  private List<RoleRights> roleRights;
 
-	public void setRoleID(Long roleID) {
-		this.roleID = roleID;
-	}
+  public Long getRoleID() {
+    return roleID;
+  }
 
-	public String getRoleName() {
-		return roleName;
-	}
+  public void setRoleID(Long roleID) {
+    this.roleID = roleID;
+  }
 
-	public void setRoleName(String roleName) {
-		this.roleName = roleName;
-	}
+  public String getRoleName() {
+    return roleName;
+  }
 
-	public RoleType getRoleType() {
-		return roleType;
-	}
+  public void setRoleName(String roleName) {
+    this.roleName = roleName;
+  }
 
-	public void setRoleType(RoleType roleType) {
-		this.roleType = roleType;
-	}
+  public RoleType getRoleType() {
+    return roleType;
+  }
 
-	public boolean isSupervisorFlag() {
-		return supervisorFlag;
-	}
+  public void setRoleType(RoleType roleType) {
+    this.roleType = roleType;
+  }
 
-	public void setSupervisorFlag(boolean supervisorFlag) {
-		this.supervisorFlag = supervisorFlag;
-	}
+  public boolean isSupervisorFlag() {
+    return supervisorFlag;
+  }
+
+  public void setSupervisorFlag(boolean supervisorFlag) {
+    this.supervisorFlag = supervisorFlag;
+  }
 
 }
--- scss-parent/scss-scancard/src/main/java/com/privasia/scss/scancard/service/CardService.java
+++ scss-parent/scss-scancard/src/main/java/com/privasia/scss/scancard/service/CardService.java
@@ -25,7 +25,7 @@ import com.privasia.scss.scancard.dto.SCUInfoDto;
 @Transactional
 public class CardService {
 
-  @Value("scss.client.port")
+  @Value("${scss.client.port}")
   private String scssClientPort;
 
   @Autowired
