772ffa38979f81f27fb909b5e2f93df4b85d8222
nadee158 <nadee158@gmail.com>
2016-11-07 23:23:08 +0530

    commented configuration for redis caching

--- scss-parent/scss-core/src/main/java/com/privasia/scss/core/security/handler/SCSSAuthenticationSuccessHandler.java
+++ scss-parent/scss-core/src/main/java/com/privasia/scss/core/security/handler/SCSSAuthenticationSuccessHandler.java
@@ -31,48 +31,58 @@ import com.privasia.scss.core.security.model.token.JwtTokenFactory;
  */
 @Component
 public class SCSSAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-	
-	private final ObjectMapper mapper;
-    private final JwtTokenFactory tokenFactory;
 
-    @Autowired
-    public SCSSAuthenticationSuccessHandler(final ObjectMapper mapper, final JwtTokenFactory tokenFactory) {
-        this.mapper = mapper;
-        this.tokenFactory = tokenFactory;
+  private final ObjectMapper mapper;
+  private final JwtTokenFactory tokenFactory;
+
+  @Autowired
+  public SCSSAuthenticationSuccessHandler(final ObjectMapper mapper, final JwtTokenFactory tokenFactory) {
+    this.mapper = mapper;
+    this.tokenFactory = tokenFactory;
+  }
+
+  @Override
+  public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+      Authentication authentication) throws IOException, ServletException {
+    UserContext userContext = (UserContext) authentication.getPrincipal();
+
+    JwtToken accessToken = tokenFactory.createAccessJwtToken(userContext);
+    JwtToken refreshToken = tokenFactory.createRefreshToken(userContext);
+
+    Map<String, String> tokenMap = new HashMap<String, String>();
+    tokenMap.put("token", accessToken.getToken());
+    tokenMap.put("refreshToken", refreshToken.getToken());
+
+    String userName = addTokenDetailsToCache(accessToken.getToken(), userContext.getUsername());
+
+    response.setStatus(HttpStatus.OK.value());
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    mapper.writeValue(response.getWriter(), tokenMap);
+
+    clearAuthenticationAttributes(request);
+  }
+
+  // @CachePut(cacheNames = "usetName", key = "#token")
+  public String addTokenDetailsToCache(String token, String userName) {
+    System.out.println("token :" + token);
+    System.out.println("userName :" + userName);
+    return userName;
+  }
+
+
+  /**
+   * Removes temporary authentication-related data which may have been stored in the session during
+   * the authentication process..
+   * 
+   */
+  protected final void clearAuthenticationAttributes(HttpServletRequest request) {
+    HttpSession session = request.getSession(false);
+
+    if (session == null) {
+      return;
     }
 
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-            Authentication authentication) throws IOException, ServletException {
-        UserContext userContext = (UserContext) authentication.getPrincipal();
-        
-        JwtToken accessToken = tokenFactory.createAccessJwtToken(userContext);
-        JwtToken refreshToken = tokenFactory.createRefreshToken(userContext);
-        
-        Map<String, String> tokenMap = new HashMap<String, String>();
-        tokenMap.put("token", accessToken.getToken());
-        tokenMap.put("refreshToken", refreshToken.getToken());
-
-        response.setStatus(HttpStatus.OK.value());
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        mapper.writeValue(response.getWriter(), tokenMap);
-
-        clearAuthenticationAttributes(request);
-    }
-
-    /**
-     * Removes temporary authentication-related data which may have been stored
-     * in the session during the authentication process..
-     * 
-     */
-    protected final void clearAuthenticationAttributes(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-
-        if (session == null) {
-            return;
-        }
-
-        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
-    }
+    session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+  }
 
 }
