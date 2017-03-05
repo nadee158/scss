4ef24c815e0f7d2bddfdfebc22e01ce6ddf039c5
Nadeeshani Senevirathna <nadee158@gmail.com>
2017-03-05 21:14:47 +0530

    modified

---
+++ scss-parent/scss-core/src/main/java/com/privasia/scss/core/repository/WHODDRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.privasia.scss.core.repository;
+
+
+import com.privasia.scss.core.model.WHODD;
+
+/**
+ * @author Janaka
+ *
+ */
+public interface WHODDRepository extends BaseRepository<WHODD, Long> {
+
+
+}
