b07b1102f186a0d8a2ae1d604ff574310b77e71c
Nadeeshani <nadee158@gmail.com>
2017-05-01 21:55:51 +0530

    modified

--- scss-parent/scss-core/src/main/java/com/privasia/scss/core/predicate/ExportsPredicates.java
+++ scss-parent/scss-core/src/main/java/com/privasia/scss/core/predicate/ExportsPredicates.java
@@ -3,11 +3,17 @@
  */
 package com.privasia.scss.core.predicate;
 
+import java.time.LocalDateTime;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
+import com.privasia.scss.common.enums.ContainerFullEmptyType;
+import com.privasia.scss.common.enums.SolasInstructionType;
 import com.privasia.scss.common.enums.TransactionStatus;
+import com.privasia.scss.core.model.HPABBooking;
 import com.privasia.scss.core.model.QExports;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Predicate;
 
 /**
@@ -43,6 +49,47 @@ public final class ExportsPredicates {
     }
   }
 
+  public static Predicate timeGateInBetween(LocalDateTime startDate, LocalDateTime endDate) {
+    if (startDate == null && endDate == null) {
+      return QExports.exports.baseCommonGateInOutAttribute.timeGateIn.isNull();
+    } else if (startDate == null) {
+      return QExports.exports.baseCommonGateInOutAttribute.timeGateIn.loe(endDate);
+    } else if (endDate == null) {
+      return QExports.exports.baseCommonGateInOutAttribute.timeGateIn.goe(startDate);
+    }
+    return QExports.exports.baseCommonGateInOutAttribute.timeGateIn.between(startDate, endDate);
+  }
 
+  public static Predicate bySolasInstructionType(SolasInstructionType vgmInstructionTerminal) {
+    if (vgmInstructionTerminal == null) {
+      return QExports.exports.solas.solasInstruction.isNull();
+    } else {
+      return QExports.exports.solas.solasInstruction.eq(vgmInstructionTerminal);
+    }
+  }
+
+  public static Predicate byWithinTolerance(boolean withinTolerance) {
+    return QExports.exports.withinTolerance.eq(withinTolerance);
+  }
+
+  public static Predicate byHpabBooking(HPABBooking hpabBooking) {
+    if (hpabBooking == null) {
+      return QExports.exports.baseCommonGateInOutAttribute.hpabBooking.isNull();
+    } else {
+      return QExports.exports.baseCommonGateInOutAttribute.hpabBooking.bookingID.eq(hpabBooking.getBookingID());
+    }
+  }
+
+  public static Predicate byContainerFullOrEmpty(ContainerFullEmptyType containerFullEmptyType) {
+    if (containerFullEmptyType == null) {
+      return QExports.exports.container.containerFullOrEmpty.isNull();
+    } else {
+      return QExports.exports.container.containerFullOrEmpty.eq(containerFullEmptyType);
+    }
+  }
+
+  public static OrderSpecifier<Long> orderByPrintEirAsc() {
+    return QExports.exports.printEir.printEIRID.asc();
+  }
 
 }
