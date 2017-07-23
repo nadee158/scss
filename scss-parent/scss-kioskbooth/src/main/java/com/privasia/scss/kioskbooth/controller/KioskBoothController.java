6cece00ff0391a94403686039daa65679103f034
Nadeeshani Senevirathna <nadeeshani.senevirathna@lk.pwc.com>
2017-07-23 23:29:21 +0530

    modified

--- scss-parent/scss-kioskbooth/src/main/java/com/privasia/scss/kioskbooth/controller/KioskBoothController.java
+++ scss-parent/scss-kioskbooth/src/main/java/com/privasia/scss/kioskbooth/controller/KioskBoothController.java
@@ -1,5 +1,6 @@
 package com.privasia.scss.kioskbooth.controller;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +27,8 @@ public class KioskBoothController {
   private KioskBoothService kioskBoothService;
 
 
-  @RequestMapping(value = "/activatetrx", method = RequestMethod.PUT,
-      produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  @RequestMapping(value = "/activatetrx", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+      consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
   public CustomResponseEntity<ApiResponseObject<?>> activateBoothsByKioskId(
       @RequestBody KioskBoothRightsDTO kioskBoothRightsDTO) {
     String status = kioskBoothService.activateBoothsForTransaction(kioskBoothRightsDTO);
@@ -35,18 +36,20 @@ public class KioskBoothController {
         HttpStatus.OK);
   }
 
-  @RequestMapping(value = "/locktrx", method = RequestMethod.PUT,
-      produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public CustomResponseEntity<ApiResponseObject<?>> lockBoothForKiosk(@RequestBody KioskBoothRightsDTO kioskBoothRightsDTO) {
+  @RequestMapping(value = "/locktrx", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+      consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public CustomResponseEntity<ApiResponseObject<?>> lockBoothForKiosk(
+      @RequestBody KioskBoothRightsDTO kioskBoothRightsDTO) {
     String status = kioskBoothService.lockBoothTransaction(kioskBoothRightsDTO);
     return new CustomResponseEntity<ApiResponseObject<?>>(new ApiResponseObject<String>(HttpStatus.OK, status),
         HttpStatus.OK);
   }
 
-  @RequestMapping(value = "/completetrx", method = RequestMethod.PUT,
-      produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public CustomResponseEntity<ApiResponseObject<?>> completekioskbooth(@RequestBody KioskBoothRightsDTO kioskBoothRightsDTO) {
-	  String status = kioskBoothService.completeBoothTransaction(kioskBoothRightsDTO);
+  @RequestMapping(value = "/completetrx", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+      consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public CustomResponseEntity<ApiResponseObject<?>> completekioskbooth(
+      @RequestBody KioskBoothRightsDTO kioskBoothRightsDTO) {
+    String status = kioskBoothService.completeBoothTransaction(kioskBoothRightsDTO);
     return new CustomResponseEntity<ApiResponseObject<?>>(new ApiResponseObject<String>(HttpStatus.OK, status),
         HttpStatus.OK);
   }
@@ -54,20 +57,22 @@ public class KioskBoothController {
   @RequestMapping(value = "/lockedkioskboothinfo/{kioskID}", method = RequestMethod.GET,
       produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
   public CustomResponseEntity<ApiResponseObject<?>> getLockedKioskBoothInfo(@PathVariable Long kioskID) {
-    KioskBoothRightsDTO kioskBoothRightDTO = kioskBoothService.getLockedKioskBoothInfo(kioskID); 
+    KioskBoothRightsDTO kioskBoothRightDTO = kioskBoothService.getLockedKioskBoothInfo(kioskID);
     return new CustomResponseEntity<ApiResponseObject<?>>(
         new ApiResponseObject<KioskBoothRightsDTO>(HttpStatus.OK, kioskBoothRightDTO), HttpStatus.OK);
   }
 
   @RequestMapping(value = "/boothaccessright", method = RequestMethod.PUT,
       produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public CustomResponseEntity<ApiResponseObject<?>> getBoothAccessRight(@RequestBody KioskBoothRightsDTO kioskBoothRightsDTO) {
-	
-	Long boothID = kioskBoothRightsDTO.getBoothClientID(); 
-	Long kioskID = kioskBoothRightsDTO.getKioskClientID();
-	Long cardNumber= kioskBoothRightsDTO.getCardNumber(); 
-	
-    List<KioskBoothRightsDTO> kioskBoothRightInfos = kioskBoothService.getBoothAccessRight(boothID, kioskID, cardNumber);  
+  public CustomResponseEntity<ApiResponseObject<?>> getBoothAccessRight(
+      @RequestBody KioskBoothRightsDTO kioskBoothRightsDTO) {
+
+    Long boothID = kioskBoothRightsDTO.getBoothClientID();
+    Long kioskID = kioskBoothRightsDTO.getKioskClientID();
+    Long cardNumber = kioskBoothRightsDTO.getCardNumber();
+
+    List<KioskBoothRightsDTO> kioskBoothRightInfos =
+        kioskBoothService.getBoothAccessRight(boothID, kioskID, cardNumber);
     return new CustomResponseEntity<ApiResponseObject<?>>(
         new ApiResponseObject<List<KioskBoothRightsDTO>>(HttpStatus.OK, kioskBoothRightInfos), HttpStatus.OK);
   }
@@ -78,6 +83,24 @@ public class KioskBoothController {
     List<ClientDTO> clientDTO = kioskBoothService.getKioskListByBooth(boothID);
     return new CustomResponseEntity<ApiResponseObject<?>>(
         new ApiResponseObject<List<ClientDTO>>(HttpStatus.OK, clientDTO), HttpStatus.OK);
+  }
+
+  @RequestMapping(
+      value = {"/searchkioskbooth/{cardNumber}/[{kioskLockStatusList}]", "/searchkioskbooth/{cardNumber}",
+          "/searchkioskbooth/[{kioskLockStatusList}]"},
+      method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+      consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public ResponseEntity<ApiResponseObject<?>> searchKioskBooth(
+      @PathVariable(value = "cardNumber", required = false) Long cardNumber,
+      @PathVariable(value = "kioskLockStatusList", required = false) String[] kioskLockStatusArray) {
+    List<String> kioskLockStatusList = null;
+    if (!(kioskLockStatusArray == null)) {
+      kioskLockStatusList = Arrays.asList(kioskLockStatusArray);
+    }
+    List<KioskBoothRightsDTO> kioskBoothRightsDTOs =
+        kioskBoothService.searchKioskBooth(cardNumber, kioskLockStatusList);
+    return new CustomResponseEntity<ApiResponseObject<?>>(
+        new ApiResponseObject<List<KioskBoothRightsDTO>>(HttpStatus.OK, kioskBoothRightsDTOs), HttpStatus.OK);
   }
 
 }
